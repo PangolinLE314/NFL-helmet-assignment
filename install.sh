28ac0e3928485c7b0b5f185f98b61501e196c35d
Thuc Trinh LE <thuc@storelift.co>
2022-04-09 23:11:14 +0200

    perf: add script to download deepsort model

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ sudo apt install ffmpeg
 cd app/modules/easydict
 python3 setup.py install
 cd ../../..
+wget https://raw.githubusercontent.com/Sharpiless/Yolov5-Deepsort/main/deep_sort/deep_sort/deep/checkpoint/ckpt.t7 -P models/deepsort
